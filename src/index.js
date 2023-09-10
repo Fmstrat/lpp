d3bfc03ad82c6ee4243eb47b4eab9a7e4f6893f2
muddybulldog <42@mdyb.xyz>
2023-09-10 11:38:03 -0400

    remove errant debug output

--- src/index.js
+++ src/index.js
@@ -138,7 +138,6 @@ async function main() {
       password: localPassword,
     };
     let user = await localClient.login(loginForm);
-    console.log (`${popularityExemption}`)
     let posts = await getPosts();
     let l = posts.length;
     console.log(`Purging ${l} posts older than ${purgeOlderThanDays} days`);
